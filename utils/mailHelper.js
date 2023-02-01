e4175ac153bf506b12dabe28452efb9cdc4da0b3
Bipul Mondal <111245325+BipulMondal@users.noreply.github.com>
2023-02-01 05:13:35 +0000

    created  a mail utility

---
+++ utils/mailHelper.js
@@ -0,0 +1,18 @@
+import Transporter from "../config/transporter.config.js";
+import config from "../config/index.js"
+import transporter from "../config/transporter.config.js";
+
+const mailHelper = async (options) => {
+    const message = {
+        from: config.SMTP_MAIL_EMAIL,   // sender address
+        to: options.email,              // list of receivers
+        subject: options.subject,       // Subject line
+        text: options.text,             // plain text body
+        // html: "<b>Hello world?</b>", // html body
+    }
+
+    await transporter.sendMail(message)
+}
+
+
+export default mailHelper
