bb9951f55e3ec037fe30f76ee399565951e0bc22
Bipul Mondal <111245325+BipulMondal@users.noreply.github.com>
2023-02-04 15:05:32 +0000

    assignment for studying

---
+++ controllers/coupons.controller.js
@@ -0,0 +1,35 @@
+/*************************************************************************
+ * @CREATE_COUPON
+ * @route http://localhost:4000/api/coupon
+ * @description Controller used for creating a new coupon 
+ * @description Only Admin and Moderator can create the coupon
+ * @returns Coupon Object with success message "Coupon Created Successfully"
+*************************************************************************/
+
+
+
+/*************************************************************************
+ * @DEACTIVATE_COUPON
+ * @route http://localhost:4000/api/coupon/deactivate/:couponId
+ * @description Controller used for deactiviting the coupon 
+ * @description Only Admin and Moderator can update the coupon
+ * @returns Coupon Object with success message "Coupon Deactivated Successfully"
+*************************************************************************/
+
+
+/*************************************************************************
+ * @DELETE_COUPON
+ * @route http://localhost:4000/api/coupon/:couponId
+ * @description Controller used for deleting the coupon 
+ * @description Only admin and Moderator can delete the coupon
+ * @returns Coupon Object with success message "Coupon Deleted Successfully"
+*************************************************************************/
+
+
+/*************************************************************************
+ * @GTE_ALL_COUPON
+ * @route http://localhost:4000/api/coupon
+ * @description Controller used for getting all coupons details
+ * @description Only admin and Moderator can get all coupon details
+ * @returns allCoupons Object 
+*************************************************************************/
